0cf6d1916a9990c04d63773844dd750f9348764a
Sergio <sergiogcosgaya@gmail.com>
2025-12-30 15:18:51 +0100

    Add small spacers in dock

--- src/os/scripts/set_dock_apps.sh
+++ src/os/scripts/set_dock_apps.sh
@@ -3,37 +3,33 @@
 # Remove all existing Dock items
 dockutil --remove all --no-restart
 
-# Group 1
 dockutil --add "/Applications/Spark.app" --no-restart
 dockutil --add "/Applications/Calendars.app" --no-restart
+dockutil --add "/Applications/Slack.app" --no-restart
+dockutil --add "/Applications/Microsoft Teams.app" --no-restart
 
-# Separator in apps section
-dockutil --add '' --type spacer --section apps --no-restart
+dockutil --add '' --type small-spacer --section apps --no-restart
 
-# Group 2
 dockutil --add "/Applications/Google Chrome.app" --no-restart
 dockutil --add "/Applications/Safari.app" --no-restart
+
+dockutil --add '' --type small-spacer --section apps --no-restart
+
 dockutil --add "/Applications/Notion.app" --no-restart
 dockutil --add "/Applications/Raindrop.app" --no-restart
+
+dockutil --add '' --type small-spacer --section apps --no-restart
+
 dockutil --add "/Applications/Sourcetree.app" --no-restart
 dockutil --add "/Applications/Visual Studio Code.app" --no-restart
-dockutil --add "/Applications/Beekeeper Studio.app" --no-restart
-dockutil --add "/Applications/Termius.app" --no-restart
 dockutil --add "/Applications/Warp.app" --no-restart
 dockutil --add "/Applications/iTerm.app" --no-restart
+dockutil --add "/Applications/Warp.app" --no-restart
+dockutil --add "/Applications/Termius.app" --no-restart
+dockutil --add "/Applications/Beekeeper Studio.app" --no-restart
 
-# Separator
-dockutil --add '' --type spacer --section apps --no-restart
+dockutil --add '' --type small-spacer --section apps --no-restart
 
-# Group 3
-dockutil --add "/Applications/Slack.app" --no-restart
 dockutil --add "/Applications/1Password.app" --no-restart
-
-# Separator
-dockutil --add '' --type spacer --section apps --no-restart
-
-# System group
 dockutil --add "/Applications/App Store.app" --no-restart
 dockutil --add "/System/Applications/System Settings.app"
-
-# Done — Dock will restart after last command
