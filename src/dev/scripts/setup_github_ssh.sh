2ad8fffc54b661f3ea6df3906c6cad5085f9daf5
Sergio <sergiogcosgaya@gmail.com>
2025-12-31 00:46:32 +0100

    feat: improve script for Github SSH key setup

--- src/dev/scripts/setup_github_ssh.sh
+++ src/dev/scripts/setup_github_ssh.sh
@@ -10,5 +10,14 @@ chown $USER ~/.ssh/github_id_rsa*
 chmod 600 ~/.ssh/github_id_rsa
 chmod 644 ~/.ssh/github_id_rsa.pub
 
+cat <<EOF > ~/.ssh/config
+Host *
+  UseKeychain yes
+  AddKeysToAgent yes
+  IdentityFile ~/.ssh/github_id_rsa
+EOF
+chmod 600 ~/.ssh/config
+
+killall ssh-agent 2>/dev/null
 eval "$(ssh-agent -s)"
-ssh-add -K ~/.ssh/github_id_rsa
+ssh-add --apple-use-keychain ~/.ssh/github_id_rsa
